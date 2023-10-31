3c8fdddbe91ce35e8ad25018108361ca922d3e9c
kaitangchang20 <149052683+kaitangchang20@users.noreply.github.com>
2023-10-31 15:21:44 +0800

    Update PassengerTypeRepositoryImpl.java

--- masterfile/src/main/java/com/caj/infra/repository/PassengerTypeRepositoryImpl.java
+++ masterfile/src/main/java/com/caj/infra/repository/PassengerTypeRepositoryImpl.java
@@ -34,12 +34,16 @@ public class PassengerTypeRepositoryImpl implements PassengerTypeRepository {
 	}
 
 	/**
-	 * 取得折扣率
-	 */
-	@Override
-	public BigDecimal findPassengerDiscount(String passengerType) {
-		return 	passengerTypeDao.findById(passengerType).get().getDiscount();
-	}
+         * 取得折扣率
+         */
+        @Override
+        public BigDecimal findPassengerDiscount(String passengerType) {
+         Optional<PassengerTypePo> result = passengerTypeDao.findById(passengerType);
+         if (result.isPresent()) {
+          return result.get().getDiscount();
+         };
+         return null;
+        }
 
 
 }
